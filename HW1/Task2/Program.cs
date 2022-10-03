8f7aea0c7f4c677ccecbdf2f273868e8ed5081c7
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-03 11:08:34 +0300

    Task2 added

---
+++ HW1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Print first number: ");
+int Number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Print second number: ");
+int Number2 = int.Parse(Console.ReadLine());
+if (Number1 > Number2)
+{
+    Console.WriteLine("max = " + Number1);
+}
+if (Number2 > Number1)
+{
+    Console.WriteLine("max = " + Number2);
+}
+if (Number1 == Number2)
+{
+    Console.WriteLine ("Numbers are equal.");
+}
