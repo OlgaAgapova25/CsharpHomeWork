d1f7dae86a30899c010f74ba1dab5d6ae1a4d342
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-03 11:45:27 +0300

    Task6 added

---
+++ HW1/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Print number: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
