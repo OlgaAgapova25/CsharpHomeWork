6e1dd03b60a2debe82903f8633f3f9173d5433d7
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-03 11:39:17 +0300

    Task4 added

---
+++ HW1/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write ("Print first number: ");
+int Number0 = int.Parse(Console.ReadLine());
+Console.Write ("Print second number: ");
+int Number1 = int.Parse(Console.ReadLine());
+Console.Write ("Print third number: ");
+int Number2 = int.Parse(Console.ReadLine());
+if (Number0 > Number1 & Number0 > Number2)
+{
+    Console.WriteLine(Number0);
+}
+if (Number1 > Number0 & Number1 > Number2)
+{
+    Console.WriteLine(Number1);
+}
+if (Number2 > Number1 & Number2 > Number0)
+{
+    Console.WriteLine(Number2);
+}
