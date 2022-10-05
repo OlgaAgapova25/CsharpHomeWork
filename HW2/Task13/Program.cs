f3187d6cfd5e8bba4ee17ce96824d56fc15e2416
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-05 17:45:52 +0300

    Task13 added

--- HW2/Task13/Program.cs
+++ HW2/Task13/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.Write("Print a number: ");
+int Number = Math.Abs(int.Parse(Console.ReadLine()));
+if (Number < 100) Console.Write("There's no third digit");
+else
+{
+    while (Number >=1000)
+    {
+        Number = Number/10;
+    }
+int Symbol3 = Number%10;
+Console.Write(Symbol3);
+}
