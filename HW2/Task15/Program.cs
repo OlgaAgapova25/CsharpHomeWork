d17096848ce5e8ef9b11b2239f98f0baef4f03b3
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-05 17:26:43 +0300

    Task15 added

--- HW2/Task15/Program.cs
+++ HW2/Task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear();
+Console.Write("Print day number: ");
+int DayNumber = int.Parse(Console.ReadLine());
+if (DayNumber == 6 || DayNumber == 7) Console.WriteLine("Yes");
+else if (DayNumber >= 1 && DayNumber <= 5) Console.WriteLine("No");
+else Console.WriteLine("error: Incorrect number");
