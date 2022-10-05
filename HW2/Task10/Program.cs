18e21150fa38341720e271c03bde6847877d6123
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-05 17:18:15 +0300

    Task10 added

---
+++ HW2/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Print 3-digit number: ");
+int Number = int.Parse(Console.ReadLine());
+int Symbol2 = Number / 10 % 10;
+Console.Write(Symbol2);
