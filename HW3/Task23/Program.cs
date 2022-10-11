f0a48b3db18c1db09cd174fdd89b557d36dbb949
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-11 18:03:57 +0300

    Task23 added

--- HW3/Task23/Program.cs
+++ HW3/Task23/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Print a positive integer: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+while (count < number)
+{
+    Console.Write(Math.Pow(count,3)+ ", ");
+    count++;
+}
+Console.Write(Math.Pow(count,3));
