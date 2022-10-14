042fa25e66aba899012b7c838e44878a10fa8f99
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-14 13:31:47 +0300

    Task27 added

--- HW4/Task27/Program.cs
+++ HW4/Task27/Program.cs
@@ -2,3 +2,45 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Clear();
+Console.WriteLine(Task27Result(DigitsCalculation(NumberInput())));
+
+int NumberInput()
+{
+    Console.WriteLine("print an integer: ");
+    int number = Math.Abs(int.Parse(Console.ReadLine()));
+    return number;
+}
+
+int[] DigitsCalculation(int number)
+{
+    int number_of_digits = 0;
+    int number_ = number;
+    while (number_ >= 1)
+    {
+        number_ /=10;
+        number_of_digits++;
+    }
+    int[] digits_mirror = new int[number_of_digits];
+    int index = 0;
+    while(index < number_of_digits)
+    {
+        digits_mirror[index] = number % 10;
+        number /= 10;
+        index++;
+    }
+    return digits_mirror;
+}
+
+int Task27Result(int[] digits)
+{
+    int sum = 0;
+    int index = 0;
+    while (index < digits.Length)
+    {
+        sum += digits[index];
+        index++;
+    }
+    return sum;
+}
--- HW4/Task29/Program.cs
+++ HW4/Task29/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
 // 6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+
+Console.Clear();
+PrintArray(FillArray());
+
+int[] FillArray ()
+{
+    int number = 8;
+    int[] arr = new int[number];
+    int length = number;
+    int index = 0;
+    while (index < length)
+    {
+        arr[index] = new Random().Next(1,100);
+        index++;
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    int index = 0;
+    int length = array.Length;
+    while (index < length)
+    {
+        Console.Write($"{array[index]}  ");
+    }
+    Console.WriteLine();
+}
+
