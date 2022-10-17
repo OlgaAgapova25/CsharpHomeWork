7f6f9367b9cb5af23e7de20fc9aae10f26e9557b
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-17 11:13:54 +0300

    Task34 solution added

---
+++ HW5/Task34/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int numberOfElements = 10;
+int minValue = 100;
+int maxValue = 999;
+
+int[] array = FillArray(numberOfElements, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine($" -> {Main(array)}");
+
+int[] FillArray (int number, int min, int max)
+{
+    int[] arr = new int[number];
+    int length = number;
+    int index = 0;
+    while (index < length)
+    {
+        arr[index] = new Random().Next(min,max+1);
+        index++;
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    int index = 0;
+    int length = array.Length;
+    while (index < length)
+    {
+        if (index == 0) Console.Write($"[{array[index]}, ");
+        else if (index == length - 1) Console.Write($"{array[index]}]");
+        else Console.Write($"{array[index]}, ");
+        index++;
+    }
+}
+
+int Main(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0) 
+        {
+            count ++;
+        }
+    }
+    return count;
+}
