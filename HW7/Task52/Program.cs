677c1811181465e6e1d7ddd5e38876f1beb41f36
Olga Agapova <ermolina.olga.l@gmail.com>
2022-10-27 18:21:45 +0300

    task52 added

--- HW7/Task52/Program.cs
+++ HW7/Task52/Program.cs
@@ -4,3 +4,59 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Main();
+
+void Main()
+{
+    int rows = 3;
+    int columns = 4;
+    int minValue = -10;
+    int maxValue = 10;
+    int[,] array = FillArray(rows, columns, minValue, maxValue);
+    PrintArray(array);
+    Console.WriteLine();
+    ColumnsAverage(array);
+}
+
+int[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    int rowsNumber = array.GetLength(0);
+    int columnsNumber = array.GetLength(1);
+    for (int i = 0; i < rowsNumber; i++)
+    {
+        for (int j = 0; j < columnsNumber; j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnsAverage (int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+           sum += array[i,j]; 
+        }
+        double average = Math.Round(sum/ array.GetLength(0),1);
+        Console.Write ($"{average} ");
+    }
+}
