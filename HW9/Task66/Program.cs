64ff2beb22da0b50f125826a5d547c46c640019a
Olga Agapova <ermolina.olga.l@gmail.com>
2022-11-12 22:10:12 +0300

    Task66 solution added

---
+++ HW9/Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Main();
+
+void Main()
+{
+    int m = InputM();
+    int n = InputN();
+    if (m > n) 
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    Console.WriteLine(Sum(m,n));
+}
+
+int InputM()
+{
+    Console.WriteLine("Print first number: ");
+    int m = int.Parse(Console.ReadLine());
+    return m;
+}
+
+int InputN()
+{
+    Console.WriteLine("Print second number: ");
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+
+int Sum (int a, int b)
+{
+    if (a == b) return a;
+    return a + Sum (a+1, b);   
+}
